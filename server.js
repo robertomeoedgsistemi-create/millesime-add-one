124b4ce5a2dd5e27dbf6b6e303b808c59ddbcecc
Your Name <you@example.com>
2026-05-19 18:41:51 +0200

    Aggiunge CORS per POS Display item

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 // ======================================================
 // CONFIGURAZIONE NEGOZI / LOCATION
 // ======================================================
